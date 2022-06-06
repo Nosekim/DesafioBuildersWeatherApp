cdaa2d83508b0e13556c170bfaee95220c7f1e3a
Nosekim <rogerfelipensk@gmail.com>
2022-06-06 03:04:34 -0300

    Removed function not used

--- src/utils/locationPermissions.ts
+++ src/utils/locationPermissions.ts
@@ -3,33 +3,6 @@ import { check, request, PERMISSIONS, RESULTS } from 'react-native-permissions';
 
 export const checkLocationPermission = async () => await PermissionsAndroid.check('android.permission.ACCESS_FINE_LOCATION');
 
-export const checkIosLocationPermission = async () => {
-  check(PERMISSIONS.IOS.LOCATION_ALWAYS)
-    .then((result) => {
-      switch (result) {
-        case RESULTS.UNAVAILABLE:
-          console.log('This feature is not available (on this device / in this context)');
-          break;
-        case RESULTS.DENIED:
-          console.log('The permission has not been requested / is denied but requestable');
-          
-          break;
-        case RESULTS.LIMITED:
-          console.log('The permission is limited: some actions are possible');
-          break;
-        case RESULTS.GRANTED:
-          console.log('The permission is granted');
-          break;
-        case RESULTS.BLOCKED:
-          console.log('The permission is denied and not requestable anymore');
-          break;
-      }
-    })
-    .catch((error) => {
-      console.log(error)
-    });
-}
-
 const hasLocationPermissionIOS: () => Promise<boolean> = async () => {
   try {
     const permission = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE)
